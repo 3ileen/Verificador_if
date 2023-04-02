aa939ff99edad29e41a5722c80bffabecefa3d7e
Eileen <65795781+3ileen@users.noreply.github.com>
2023-04-01 23:51:22 -0500

    Add files via upload

---
+++ verificador.py
@@ -0,0 +1,18 @@
+import ast
+
+def sentencia_if(expression):
+    try:
+        # Analizar la sentencia y crear el AST correspondiente
+        ast.parse(expression)
+        return True
+    except SyntaxError:
+        return False
+    
+# ejemplo : "if x > 0:\n    print('x es positivo')"
+sentencia = "if x > 0:"
+#sentencia = input("Ingresa una sentencia:")
+if sentencia_if(sentencia):
+    #print("La sentencia no pertenece al lenguaje de Python o esta sintacticamente mal.")
+    print("La sentencia si pertenece al lenguaje de Python.")
+else:
+    print("La sentencia no pertenece al lenguaje de Python o esta sintacticamente mal.")
